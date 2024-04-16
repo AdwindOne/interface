ccbc2ca5b71ecf1c1fbaff13b6566ca25fb0c2f6
via <overfor@icloud.com>
2024-04-16 16:18:55 +0800

    update link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -351,7 +351,7 @@ export default function Header() {
           <StyledNavLink id={`stake-nav-link`} to={'/vote'}>
             Vote
           </StyledNavLink>
-          <StyledExternalLink id={`stake-nav-link`} href={'https://adwindone.github.io'}>
+          <StyledExternalLink id={`stake-nav-link`} href={'https://adwindone.github.io/interface/#/swap'}>
             Charts <span style={{ fontSize: '11px' }}>↗</span>
           </StyledExternalLink>
         </HeaderLinks>
